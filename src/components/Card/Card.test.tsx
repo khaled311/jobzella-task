6d0de5bb3a6a6a30a45e9b5b303fc491fe23c54f
khaled311 <k.sarhan311@gmail.com>
2023-02-26 02:05:15 +0200

    fix: vercel build

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -3,13 +3,27 @@ import Card from "./Card";
 
 describe("Card", () => {
   it("should have item name", () => {
-    render(<Card item={{ name: "Khaled" }} />);
+    render(
+      <Card
+        item={{ name: "Khaled" } as any}
+        cardColor={undefined as any}
+        status={undefined as any}
+        key={undefined}
+      />
+    );
 
     expect(screen.getByText("Khaled")).toBeInTheDocument();
   });
 
   it("check item status", () => {
-    render(<Card status="todo" />);
+    render(
+      <Card
+        status="todo"
+        item={undefined as any}
+        cardColor={undefined as any}
+        key={undefined as any}
+      />
+    );
 
     expect(
       screen.getByTestId("progressBar").classList.contains("w-[0px]")
